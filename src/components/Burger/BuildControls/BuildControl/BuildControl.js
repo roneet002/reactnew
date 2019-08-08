f6e7958afbf9d7dcd11062935f45eb474d148e7e
roneet002 <roneet.kumar002@gmail.com>
2019-08-08 22:46:28 +0530

    add button added

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ return(
 <div className="clearfix text-center mx-auto w-25">
 <p className="float-left mr-4 w-25 text-left color">{props.label} :</p>
 <button type="button" className="float-left btn btn-outline-dark mr-4">Less</button>
-<button type="button" className="float-left btn btn-outline-dark mr-4" onClick={props.ingrediant}>More</button>
+<button type="button" className="float-left btn btn-outline-dark mr-4" onClick={props.added}>More</button>
 </div>
 )
 }
