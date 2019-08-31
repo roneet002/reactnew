89c0147007f57676b3e151c57545bfc040d3a48c
Amit Kumar <roneet.kumar002@gmail.com>
2019-08-31 15:05:28 +0530

    abcd

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,9 @@ const BuildControl = (props)=>{
 return(
 <div className="clearfix text-center mx-auto w-25">
 <p className="float-left mr-4 w-25 text-left color">{props.label} :</p>
-<button type="button" className="float-left btn btn-outline-dark mr-4">Less</button>
+<button type="button" className="float-left btn btn-outline-dark mr-4"
+ onClick={props.removed}
+disabled={props.disabled}>Less</button>
 <button type="button" className="float-left btn btn-outline-dark mr-4" onClick={props.added}>More</button>
 </div>
 )
